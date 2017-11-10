d9e16d3940a246b7d80e0d612b8cd30507201bac
melvyn <melvyndev@gmail.com>
2017-11-10 17:37:39 +0800

    Initial commit

---
+++ Source/Swiper.h
@@ -0,0 +1,17 @@
+//
+//  Swiper.h
+//  Swiper
+//
+//  Created by melvyn on 10/11/2017.
+//  Copyright © 2017 melvyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swiper.
+FOUNDATION_EXPORT double SwiperVersionNumber;
+
+//! Project version string for Swiper.
+FOUNDATION_EXPORT const unsigned char SwiperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swiper/PublicHeader.h>
